10231ff5e6ed58e152bc8636d07500c2a9b2379c
Pavel Kogut <kogut.kogut1@mail.ru>
2021-08-30 15:26:08 +0500

    Test

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
