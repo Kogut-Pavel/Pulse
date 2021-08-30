9887df82e116aa5dc29a5624f737b09cf6d43526
Pavel Kogut <kogut.kogut1@mail.ru>
2021-08-30 17:06:14 +0500

    Add adaptive carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
         speed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+        dotsClass: 'slick-dots',
+        dots: true,
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
-                    dots: true,
                     arrows: false
                 }
             }
