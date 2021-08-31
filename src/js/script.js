994f60cdb9b17927ee24a52e1e537c5dfb111b1f
Pavel Kogut <kogut.kogut1@mail.ru>
2021-08-31 23:09:57 +0500

    Catalog section made

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,37 @@ $(document).ready(function(){
             }
         ]  
       });
-  });   
+
+      // Tabs
+
+    const tabHeaders = document.querySelectorAll('[data-tab]');
+    const contentBoxes = document.querySelectorAll('[data-tab-content]');
+    
+    tabHeaders.forEach(function (item) {       
+        item.addEventListener('click', function () {
+            tabHeaders.forEach(function (item) {
+            item.classList.remove('catalog__tab_active');
+            });   
+            const contentBox = document.querySelector('#' + this.dataset.tab);
+            contentBoxes.forEach(function (item) {
+                item.classList.remove('catalog__content_active');
+            });
+            item.classList.add('catalog__tab_active');
+            contentBox.classList.add('catalog__content_active');
+        });
+    });
+    
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});   
+
