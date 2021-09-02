15dfc611a726911463e06da7a831b4f5a14ed428
Pavel Kogut <kogut.kogut1@mail.ru>
2021-09-02 10:15:07 +0500

    Small fixes

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         dotsClass: 'slick-dots',
         dots: true,
         responsive: [
