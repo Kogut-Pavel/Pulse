0fdf124cd9611a3a97751073f33c57da3e6d78bb
Pavel Kogut <kogut.kogut1@mail.ru>
2021-09-05 11:32:33 +0500

    Valid site

--- src/js/script.js
+++ src/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__block').eq(i).toggleClass('catalog-item__block_active');
             });
         });
     }
@@ -159,5 +159,6 @@ $(document).ready(function(){
       };
 
       smoothScrolling();
+      new WOW().init();
 });   
 
